c8fc3121197b3ed60a79bfdc4854f4a4b32252a4
Kevin Figueroa <kevmiguelarronte@gmail.com>
2023-07-23 19:41:45 -0400

    RB frame

--- library_nfl.py
+++ library_nfl.py
@@ -57,11 +57,10 @@ def pull_data_stats(urls):
     return dataframes
 
 # /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+def pull_data_snapcount(urls):
+    # Dictionary to store all tables
+    dataframes = {}
 
-
-# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-def pull_data_target_distribution(urls):
     # Loop through each url and import data
     for url in urls: 
         # The read_html function returns a list of all the tables found on the web page
@@ -70,10 +69,84 @@ def pull_data_target_distribution(urls):
         # Assuming the table you want is the first one in the list, you can access it like this
         table_df = tables[0]
 
+        # Get position from URL
+        position = url.split('/')[6].split('.')[0].upper()
+
+        # Remove %%!
+        table_df = table_df.replace('\%', '', regex=True)
+        # Remove byes!
+        table_df = table_df.replace('bye', '200', regex=True)
+
         table_df.columns = [x.lower() for x in table_df.columns]
 
+        # Keep only relevant columns
+        # table_df = table_df[['player', 'team', 'pos', 'avg']]
+
         # Convert avg to numeric
+        table_df.rename(columns={'avg': 'avg_snap_pct'}, inplace=True)
+
+        if '18' in table_df.columns:
+            int_columns = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', 'ttl','avg_snap_pct']
+        else:
+            int_columns = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', 'ttl','avg_snap_pct']
+
+
+        table_df[int_columns] = table_df[int_columns].astype(float)
+
+        table_df = table_df.replace(200, np.nan)
+
+        # Custom function to calculate the average snap count for each x when played
+        def average_snapcount(x, pos1, pos2, limit=0):
+            snapcounts = [snapcount for snapcount in x[pos1: pos2] if snapcount > limit]  # Selecting only week columns (from 3rd column onwards)
+            return sum(snapcounts) / len(snapcounts) if len(snapcounts) > 0 else None
+
+        # Create the new column with avg snap pct when played
+        if '18' in table_df.columns:
+            table_df['avg_snap_pct_played'] = table_df.apply(lambda x: average_snapcount(x, 2, 20), axis=1)
+        else: 
+            table_df['avg_snap_pct_played'] = table_df.apply(lambda x: average_snapcount(x, 2, 19), axis=1)
+
+        # Create the new column with avg snap pct global
+        if '18' in table_df.columns:
+            table_df['avg_snap_pct_global'] = table_df.apply(lambda x: average_snapcount(x, 2, 20, -1), axis=1)
+        else: 
+            table_df['avg_snap_pct_global'] = table_df.apply(lambda x: average_snapcount(x, 2, 19, -1), axis=1)
+
+        # Create position variable
+        table_df['pos'] = position
+
+        # Drop column
+        table_df.drop(columns='avg_snap_pct', inplace=True)
+
+        # Return dataframe
+        dataframes[position] = table_df
+
+    # Return dictionary 
+    return dataframes
+
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+def pull_data_target_distribution(urls, nfl_teams_dict):
+    # Loop through each url and import data
+    for url in urls: 
+        # The read_html function returns a list of all the tables found on the web page
+        tables = pd.read_html(url)
+
+        # Assuming the table you want is the first one in the list, you can access it like this
+        table_df = tables[0]
+
+        # Clean column names
+        table_df.columns = [x.lower() for x in table_df.columns]
+
+        table_df.rename(columns=lambda x: x.replace(' ', '_'), inplace=True)
+
+        table_df.rename(columns=lambda x: x.replace('%', 'pct'), inplace=True)
+
+        # Rename columns
         table_df.rename(columns={'team': 'team_name'}, inplace=True)
+
+        # Create 
+        table_df['team'] = table_df['team_name'].map(nfl_teams_dict)
 
 
     # Return dictionary 
